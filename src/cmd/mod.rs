fd4add475503679dea24cba92868f8a36a85bcbc
jxmau <jxmau.pro@proton.me>
2023-06-20 15:14:01 +0200

    Uprage of the sheet command
    * new can add sheet to every files in the current directory with the
      -all flag that replaces the path
    * Create a common function to get every sqlx file from a directory
      (PathBuff)
    * Update the readme.MD

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,2 +1,34 @@
+use std::{path::{PathBuf}};
+use log::{error, trace};
+use std::process::exit;
+
 pub mod generate;
 pub mod sheet;
+
+
+// Get all sql files from dir
+pub fn get_sqlx_files_from_dir(path: &PathBuf) -> Vec<(String, PathBuf)> {
+trace!("Dir to read : {path:?} ");
+let files = match path.read_dir() {
+    Ok(s) => s,
+    Err(e) => {
+        println!("Fatal error");
+        error!("Failed to read the entries of the directory given.");
+        trace!(" {e} ");
+        exit(1);
+    }
+};
+
+let mut list_sent: Vec<(String, PathBuf)> = Vec::new();
+for file in files {
+    if let Ok(valid_file) = file {
+        if let Ok(valid_filename) = valid_file.file_name().into_string() {
+            if valid_filename.ends_with(".sql") {
+                list_sent.push((valid_filename, valid_file.path()));
+            }
+        }
+    } 
+};
+
+list_sent
+}
--- src/cmd/sheet.rs
+++ src/cmd/sheet.rs
@@ -1,24 +1,12 @@
-use std::{process::exit, path::PathBuf, fs::File, io::{Read, Write}};
+use std::{process::exit, path::PathBuf, fs::{File}, io::{Read, Write}};
 use log::{info, error, trace};
 
-use crate::metadata::{Metadata, self};
+use crate::{metadata::{Metadata}, cmd::get_sqlx_files_from_dir};
 
 pub fn sheet(path: Option<String>) {
-    println!("{path:?}");
+    
     info!("Command chosen : sheet");
-    trace!("Verifying the path provided");
-    let path = match path {
-        Some(s) if s.ends_with(".sql")=> s,
-        Some(_) => {
-            println!("Path argument is invalid.");
-            exit(1);
-        }
-        None => {
-            println!("Path argument is missing.");
-            exit(1);
-        },
-    };
-
+    
     trace!("Fetching and verifying the pwd");
     let pwd = match std::env::current_dir() {
         Ok(pwd) if pwd.ends_with("migrations") => pwd,
@@ -34,22 +22,60 @@ pub fn sheet(path: Option<String>) {
         },
     };
 
-    let filepath = pwd.join(path);
-    trace!("Path of the file has been joined : {filepath:?}");
 
-    let content = read_file(filepath.clone());
-    write_file(filepath, content);
+    trace!("Verifying the path provided");
+    let mut files : Vec<(String, PathBuf)> = Vec::new();
+    match path {
+        Some(s) if s.ends_with(".sql")=> {
+            info!("One file to add sheet to.");
+            let mut path = PathBuf::new();
+            path.set_file_name(pwd.join(s.clone()));
+            files.push((s, path))
+        },
+        Some(s) if s.eq("--all") | s.eq("-a") => { 
+            let mut files_read = get_sqlx_files_from_dir(&pwd);
+            files.append(&mut files_read );
+        }
+        Some(_) => {
+            println!("Path argument is invalid.");
+            exit(1);
+        }
+        None => {
+            println!("Path argument is missing.");
+            exit(1);
+        },
+    };
+
+
+    for file in files {
+        let (file_name, file_path) = file;
+        trace!("Path of the file : {file_path:?}");
+        print!("  - {file_name} : ");
+        match read_file(file_path.clone()) {
+            Ok(content) => {
+                match write_file(file_path, content) {
+                    Ok(_) => (),
+                    Err(e) => println!(" {e} "),
+                }
+            
+            },
+            Err(e) => println!("{e}"), 
+        }
+        
+
+
+    }
 
 }
 
+
 // Read the file and check if there's already a sheet, return File
-pub fn read_file(path: PathBuf) -> String {
+pub fn read_file(path: PathBuf) -> Result<String, String> {
     let mut file = match File::open(path) {
         Ok(s) => s,
         Err(e) => {
-            println!("Failed to open the file.");
             trace!(" {e} ");
-            exit(1);
+            return Err("Failed to open the file.".to_string());
         },
     };
     trace!("File has been opened");
@@ -58,30 +84,31 @@ pub fn read_file(path: PathBuf) -> String {
     match file.read_to_string(&mut content) {
         Ok(_) => (),
         Err(e) => {
-            println!("Failed to read the file.");
             trace!(" {e} ");
-            exit(1);
+            return Err("Failed to read the file.".to_string());
+            
         }
     }
     trace!("File has been read");
 
     if content.starts_with("-- Table:") {
-        println!("Syntax has been found in the file that would suggest that the file has already a sheet generated.");
-        exit(1);
+        info!("Syntax has been found in the file that would suggest that the file has already a sheet generated.");
+        return Err("No change required.".to_string());
+        
     }
 
-    content
+    Ok(content)
 }
 
 // Write the file
-fn write_file(path: PathBuf, content: String) {
+fn write_file(path: PathBuf, content: String) -> Result<(), String> {
 
     let mut file = match File::create(path) {
         Ok(s) => s,
         Err(e) => {
-            println!("Failed to open the file.");
+            error!("Failed to open the file.");
             trace!(" {e} ");
-            exit(1);
+            return Err("Fatal Error".to_string())
         },
     };
     trace!("File has been opened in write-only mode.");
@@ -93,10 +120,12 @@ fn write_file(path: PathBuf, content: String) {
     match file.write_all(metadata.as_bytes()) {
         Ok(_) => println!("Sheet has been added to the file."),
         Err(e) =>  {
-            println!("Failed to write the file.");
-            error!(" {e} ");
+            error!("Failed to write the file.");
+            trace!(" {e} ");
+            return Err("Fatal Error".to_string())
         }
     }
 
     file.sync_all().unwrap();
+    Ok(())
 }
